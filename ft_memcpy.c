3eb62b95f73a5306831ea958eb1aad2953e45ad4
Sura Daabes <sdaabes@c1r4s2.42amman.com>
2025-08-13 20:49:08 +0300

    mem functions add

---
+++ ft_memcpy.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sdaabes <sdaabes@student.42amman.com>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/08 17:36:35 by sdaabes           #+#    #+#             */
+/*   Updated: 2025/08/13 20:39:23 by sdaabes          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+#include <stdio.h>
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	size_t			i;
+	unsigned char	*d;
+	unsigned char	*s;
+
+	i = 0;
+	d = (unsigned char *)dest;
+	s = (unsigned char *)src;
+	while (i < n)
+	{
+		d[i] = s[i];
+		i++;
+	}
+	return (dest);
+	
+}
+// int main()
+// {
+//     char src[] = "Hello";
+//     char dest[10];
+
+//     ft_memcpy(dest, src, 6);
+//     printf("%s\n", dest);
+// }
---
+++ ft_memmove.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sdaabes <sdaabes@student.42amman.com>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/08 18:45:58 by sdaabes           #+#    #+#             */
+/*   Updated: 2025/08/08 19:32:45 by sdaabes          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+void *ft_memmove(void *dest, const void *src, size_t n)
+{
+    unsigned char *d = (unsigned char *)dest;
+    unsigned char *s = (unsigned char *)src;
+
+    
+}
